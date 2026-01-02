65a1bd78a8c79e1a50870839d207a500854e849b
leticia.silva <frhleticia@gmail.com>
2026-01-02 11:10:30 -0300

    Testes Unitários

--- src/test/service/ServiceTest.java
+++ src/test/service/ServiceTest.java
@@ -1,13 +1,52 @@
 package service;
 
+import entity.Endereco;
+import entity.Pessoa;
+import service.Service;
+import repository.Repository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServiceTest {
 
-    //Arranjo...dado
+    static void deveLancarExcecaoQuandoNomeNulo() {
+        //Arranjo...dado
+        Repository repository = new Repository();
+        Service service = new Service(repository);
 
+        Endereco endereco = new Endereco("Rua Gomes", 231L, "Limoeiro", "Porto", "BA", "12345678");
 
-    //Ação...quando
+        //Ação...quando & Verificação...então
+        try {
+            service.criarUsuario(null, LocalDate.of(2007, 12, 31), "12345678901", endereco);
+            throw new RuntimeException(
+                    "Teste falhou: deveria ter lançado exceção");
 
+        } catch (RuntimeException e) {
+        }
+    }
 
-    //Verificação...então
+    static void deveLancarExcecaoQuandoNomeBlank() {
+        //Arranjo...dado
+        Repository repository = new Repository();
+        Service service = new Service(repository);
 
+        Endereco endereco = new Endereco("Rua Gomes", 231L, "Limoeiro", "Porto", "BA", "12345678");
+
+        //Ação...quando & Verificação...então
+        try {
+            service.criarUsuario("", LocalDate.of(2007, 12, 31), "12345678901", endereco);
+            throw new RuntimeException(
+                    "Teste falhou: deveria ter lançado exceção");
+
+        } catch (RuntimeException e) {
+        }
+    }
+
+    public static void main(String[] args) {
+        deveLancarExcecaoQuandoNomeNulo();
+        deveLancarExcecaoQuandoNomeBlank();
+    }
 }
